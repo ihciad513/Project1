732ad70da4e7ea6f188432e22255c62fb743f356
ihciad513 <ihciad5131707@gmail.com>
2021-05-12 23:42:00 +0900

    Initial Commit

---
+++ Shared/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Shared
+//
+//  Created by Daichi Sasayama on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
